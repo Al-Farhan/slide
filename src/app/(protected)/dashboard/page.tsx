0f20c657ef38482241a44261c1623902586bf9f8
Farhan Shaikh <programmingwithfarhan@gmail.com>
2025-01-17 17:54:33 +0530

    backend queries for authentication

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,14 +1,16 @@
+import { onBoardUser } from '@/actions/user';
+import { redirect } from 'next/navigation';
 import React from 'react'
 
 type Props = {}
 
-const Page = (props: Props) => {
+const Page = async (props: Props) => {
 
-    // server action Onboard the user
-    // 200 || 201
-  return (
-    <div>Page</div>
-  )
+    const user = await onBoardUser();
+    if (user.status === 200 || user.status === 201) {
+      return redirect(`/dashboard/${user.data?.firstname}${user.data?.lastname}`)
+    }
+  return redirect('/sign-in');
 }
 
 export default Page
